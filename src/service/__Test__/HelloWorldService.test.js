9f313452d64ba90b8bc544aa83fe8b3383340748
stein_korsveien@msn.com <stein_korsveien>
2019-05-21 13:35:52 +0200

    Change to ENVIRONMENT

--- src/service/__Test__/HelloWorldService.test.js
+++ src/service/__Test__/HelloWorldService.test.js
@@ -1,8 +1,8 @@
 import HelloWorldService from '../HelloWorldService'
 
 beforeAll(()=> {
-  if (process.env.ENVIROMENT === 'build') {
-     console.info("ENVIROMENT=== 'build'")
+  if (process.env.ENVIRONMENT === 'build') {
+     console.info("ENVIRONMENT=== 'build'")
      global.fetch = require('jest-fetch-mock')
      fetch.resetMocks()
      fetch.mockResponseOnce( 'Hello World')
@@ -18,19 +18,19 @@ it('should recive hellow world from the server ', async () => {
 })
 
 it('should load SERVER_URL in devekpment enviroment', () => {
-      if (process.env.ENVIROMENT === 'dev') {
+      if (process.env.ENVIRONMENT === 'dev') {
          expect(process.env.SERVER_URL).toBe('http://localhost:8080')
        }
    })
 
 it('should load SERVER_URL in build enviroment', () => {
-      if (process.env.ENVIROMENT === 'build') {
+      if (process.env.ENVIRONMENT === 'build') {
          expect(process.env.SERVER_URL).toBe('non')
        }
    })
 
 it('should load SERVER_URL in test enviroment', () => {
-      if (process.env.ENVIROMENT === 'test') {
+      if (process.env.ENVIRONMENT === 'test') {
          expect(process.env.SERVER_URL).toBe('https://todofrontend-dot-project45914.appspot.com')
        }
    })
