3ef46bcc3185e8c25a5f57df6d7433fe7f2136a9
stein_korsveien@msn.com <stein_korsveien>
2019-05-30 15:39:23 +0200

    added coole cloud logging

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 import {Component} from 'react'
 import TodoApp from './TodoApp'
-
+import Logging from '@google-cloud/logging'
+let  log
+let metaData
 export default class App extends Component { 
+    
+    constructor() { 
+       super()
+       this.createLog()
+     }
+
+     async createLog() { 
+         const logging = new Logging(process.env.GAE_APPLICATION)
+         log = logging.log('my-log')
+          metaData =   { 
+            resource:  {  type:'global' }
+          }
+      
+            const entry = log.entry(metaData, 'Log Entry')
+            await log.write(entry)
+            console.log('Hello World')
+       }
+
     render()  { 
+           
             console.info('app')
             return (<TodoApp />)
         }
--- src/service/Environment.js
+++ src/service/Environment.js
@@ -6,7 +6,7 @@
       console.log(frontendUrl)
       console.log(port)
 
-      if ((process.platform ==='linux') || (port.length !== 0)) {
+      if ((process.env.NODE_ENV === 'production') || (port.length !== 0)) {
           const backendUrl = 'http://'+ frontendUrl + ':'+ port + '/backendUrl'
           console.log(backendUrl)
           let response = await fetch(backendUrl)
