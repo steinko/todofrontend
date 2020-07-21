a990e53c5e5b9a205ea5adbe836cb9180189a1bd
steinko <stein_korsveien@msn.com>
2020-07-21 16:13:20 +0200

    change bootstrat and add deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
 import React from 'react'
 import {Component} from 'react'
 import TodoApp from './components/todo/todoapp/TodoApp'
-import  './Bootstrap.css'
 
 export default class App extends Component { 
     
-
-
     render()  { 
-            //Sending error message to server
-            //logger.error("test error message");
-           //Sending info log to server 
-           // logger.info("test info");
             console.info('app')
             return (<TodoApp />)
         }
